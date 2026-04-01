2377949cbbd56fe4aa013fe2c18fe79431f24639
Soomin <soomin02094@gmail.com>
2026-04-01 11:36:18 +0900

    Initial commit

---
+++ src/main/java/com/example/kafkaredispractice/KafkaRedisPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkaredispractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaRedisPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaRedisPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kafkaredispractice/KafkaRedisPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkaredispractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaRedisPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
